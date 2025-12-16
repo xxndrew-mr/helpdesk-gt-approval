fda34d0e19b0fc61b5412928711a44b1b5bdbbed
Andre Marshandito <andremarshandito@gmail.com>
2025-12-16 09:58:21 +0700

    update

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,33 +1,22 @@
 import { PrismaClient } from '@prisma/client';
 
-// === PERBAIKAN BIGINT (TAMBAHKAN BLOK INI) ===
-// Ini "mengajari" JSON.stringify cara menangani BigInt
-// dengan mengubahnya menjadi String.
+// === FIX BigInt JSON ===
 if (typeof BigInt.prototype.toJSON === 'undefined') {
   BigInt.prototype.toJSON = function () {
     return this.toString();
   };
 }
-// ========================================
 
-// Mendeklarasikan variabel global untuk Prisma Client
-let prisma;
+const globalForPrisma = globalThis;
 
-// --- Ini Kode Anda yang Sudah Benar ---
-// Mengecek apakah kita berada di lingkungan produksi
-if (process.env.NODE_ENV === 'production') {
-  // Di produksi, buat instance baru
-  prisma = new PrismaClient();
-} else {
-  // Di pengembangan, cek apakah instance sudah ada di global
-  if (!global.prisma) {
-    // Jika tidak ada, buat instance baru dan simpan di global
-    global.prisma = new PrismaClient();
-  }
-  // Gunakan instance dari global
-  prisma = global.prisma;
+const prisma =
+  globalForPrisma.prisma ||
+  new PrismaClient({
+    log: ['error'],
+  });
+
+if (process.env.NODE_ENV !== 'production') {
+  globalForPrisma.prisma = prisma;
 }
-// ------------------------------------
 
-// Ekspor instance Prisma Client yang sudah di-cache dan diperbaiki
 export default prisma;
