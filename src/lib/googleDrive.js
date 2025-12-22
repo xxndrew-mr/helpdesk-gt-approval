9290c555c0637259f4abfda7b0d37babf2cc855a
Andre Marshandito <andremarshandito@gmail.com>
2025-12-22 08:25:39 +0700

    Update Lib"

--- src/lib/googleDrive.js
+++ src/lib/googleDrive.js
@@ -1,50 +1,25 @@
-// lib/googleDrive.js
 import { google } from 'googleapis';
-import path from 'path';
-
-const KEYFILEPATH = path.join(process.cwd(), 'service-account-key.json');
-const SCOPES = ['https://www.googleapis.com/auth/drive.file'];
 
 const auth = new google.auth.GoogleAuth({
-  keyFile: KEYFILEPATH,
-  scopes: SCOPES,
+  credentials: {
+    project_id: process.env.GDRIVE_PROJECT_ID,
+    client_email: process.env.GDRIVE_CLIENT_EMAIL,
+    private_key: process.env.GDRIVE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  },
+  scopes: ['https://www.googleapis.com/auth/drive'],
 });
 
-export const drive = google.drive({ version: 'v3', auth });
-
-export async function getOrCreateFolder(folderName) {
-  const res = await drive.files.list({
-    q: `mimeType='application/vnd.google-apps.folder' and name='${folderName}' and trashed = false`,
-    fields: 'files(id, name)',
-    spaces: 'drive',
-  });
-
-  if (res.data.files && res.data.files.length > 0) {
-    return res.data.files[0].id;
-  }
-
-  const folder = await drive.files.create({
-    requestBody: {
-      name: folderName,
-      mimeType: 'application/vnd.google-apps.folder',
-    },
-    fields: 'id',
-  });
-
-  return folder.data.id;
-}
+export const drive = google.drive({
+  version: 'v3',
+  auth,
+});
 
 export async function makeFilePublic(fileId) {
-  try {
-    await drive.permissions.create({
-      fileId,
-      requestBody: {
-        role: 'reader',
-        type: 'anyone',
-      },
-      fields: 'id',
-    });
-  } catch (err) {
-    console.error('Gagal set public:', err);
-  }
+  await drive.permissions.create({
+    fileId,
+    requestBody: {
+      role: 'reader',
+      type: 'anyone',
+    },
+  });
 }
