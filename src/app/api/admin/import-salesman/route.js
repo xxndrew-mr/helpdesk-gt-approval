2c471f812511f30f97542b798ccccc59a97fca02
Andre Marshandito <andremarshandito@gmail.com>
2026-02-20 10:08:11 +0700

    update

--- src/app/api/admin/import-salesman/route.js
+++ src/app/api/admin/import-salesman/route.js
@@ -11,7 +11,7 @@ export async function POST(request) {
 
     if (!session || session.user.role !== 'Administrator') {
       return NextResponse.json(
-        { message: 'Tidak diizinkan Lagi' },
+        { message: 'Tidak diizinkan' },
         { status: 403 }
       );
     }
