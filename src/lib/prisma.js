3e5d9c6fd727017071bcdbc66b081d4dad88c763
Andre Marshandito <andremarshandito@gmail.com>
2026-05-13 11:28:31 +0700

    U

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,12 +1,14 @@
 import { PrismaClient } from '@prisma/client';
+import { withAccelerate } from '@prisma/extension-accelerate';
 
 const globalForPrisma = globalThis;
 
+// Gunakan ekstensi .withAccelerate()
 const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
     log: ['error'],
-  });
+  }).$extends(withAccelerate());
 
 if (process.env.NODE_ENV !== 'production') {
   globalForPrisma.prisma = prisma;
